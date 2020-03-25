0ba6b50043556ad4968b344acec36f8322933fb6
narimiran <narimiran@disroot.org>
2020-03-25 19:14:26 +0100

    Revert "t8982: smaller sleep; 2x speedup"
    This reverts commit ee1dec22802f6d4355a65d98916ffb4e3b5fe4b5.

--- tests/async/t8982.nim
+++ tests/async/t8982.nim
@@ -9,14 +9,14 @@ runForever should throw ValueError, this is expected
 import asyncdispatch
 
 proc failingAwaitable(p: int) {.async.} =
-  await sleepAsync(100)
+  await sleepAsync(500)
   if p > 0:
     raise newException(Exception, "my exception")
 
 proc main() {.async.} =
   let fut = failingAwaitable(1)
   try:
-    await fut or sleepAsync(50)
+    await fut or sleepAsync(100)
     if fut.finished:
       echo "finished"
     else:
